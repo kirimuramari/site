7743130a1689c045fe410aa8755f03b51c59d218
kirimuramari <lifemastert@gmail.com>
2018-01-09 22:36:35 +0900

    パズドラ

---
+++ mysite2/script.js
@@ -0,0 +1,17 @@
+﻿//ページトップ
+$(function(){
+  $('.toTop').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+});
+
+//スマホのみアコーディオン
+
+$(function(){
+  $('.Mboder').click(function(){
+  $(this).toggleClass('.Mtable').next().slideToggle();
+  });
+  });
